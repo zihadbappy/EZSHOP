805feeb99be54e1bbdf33f2ea899b1f4905af6b8
Zihad Bappy <zihadbappy@gmail.com>
2021-02-05 03:09:12 +0600

    1- Header and Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,20 @@
+// import React from 'react'
+import {Container} from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
 function App() {
   return (
-    <div className="App">
+    <>
+      <Header/>
+      <main className='py-3'>
+        <Container>
+
       <h1>Welcome to EzShop</h1>
-    </div>
+        </Container>
+      </main>
+      <Footer/>
+    </>
   );
 }
 
