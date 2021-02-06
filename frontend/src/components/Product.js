5c54fcc1728e6b380714b75173b15441af1646f2
Zihad Bappy <zihadbappy@gmail.com>
2021-02-07 02:22:38 +0600

    1-- DONE

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -6,7 +6,7 @@ import Rating from "./Rating"
 const Product = ({ product }) => {
   return (
     <Card className='my-3 p-3 rounded'>
-      <Link to href={`/product/${product._id}`}>
+      <Link to={`/product/${product._id}`}>
         <Card.Img src={product.image} />
       </Link>
 
