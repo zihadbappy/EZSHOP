7cc32f1e38c91fa12b3a90450a24963f8804625c
Zihad Bappy <zihadbappy@gmail.com>
2021-02-07 21:24:10 +0600

    2-- serving prods from backend and routes

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const products = require("./data/products")
+const app = express()
+
+app.get("/", (req, res) => {
+  res.send("Api go brrr")
+})
+
+app.get("/api/products", (req, res) => {
+  res.json(products)
+})
+
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((p) => p._id === req.params.id)
+  res.json(product)
+})
+
+app.listen(4000, console.log("Server running on port 4000"))
