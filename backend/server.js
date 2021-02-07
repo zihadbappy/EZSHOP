d502f5f2ceaa29ab31cd51f4e63d7043b177e745
Zihad Bappy <zihadbappy@gmail.com>
2021-02-08 03:08:15 +0600

    2-- Finished Backend setup

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,9 @@
-const express = require("express")
-const products = require("./data/products")
+import express from "express"
+import dotenv from "dotenv"
+import products from "./data/products.js"
+
+dotenv.config()
+
 const app = express()
 
 app.get("/", (req, res) => {
@@ -15,4 +19,9 @@ app.get("/api/products/:id", (req, res) => {
   res.json(product)
 })
 
-app.listen(4000, console.log("Server running on port 4000"))
+const PORT = process.env.PORT || 4000
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+)
