ccaf728eb47bd30b8bcf8051eed24ee0cded1583
Zihad Bappy <zihadbappy@gmail.com>
2021-02-13 14:15:55 +0600

    5-- half

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,17 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import "./bootstrap.min (1).css"
-import "./index.css"
-import App from "./App"
-import reportWebVitals from "./reportWebVitals"
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import store from './store'
+import './bootstrap.min (1).css'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </Provider>,
+  document.getElementById('root')
 )
 
 reportWebVitals()
